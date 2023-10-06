afa84442e642171afbe703a3f78b45326c242b62
mahmoudtaha <amer140106@gmail.com>
2023-10-06 03:34:05 +0300

    update second

--- js/aroma.js
+++ js/aroma.js
@@ -2,7 +2,7 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
-window.onscroll = function(){
+window.onscroll = function () {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 
@@ -13,7 +13,36 @@ menu.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
+var x = document.getElementsByClassName("swiper");
+var slideIndex = 0;
+var slidesPerView = 4;
+var slidesToSlide = 1;
 
+function swipeDivs() {
+    slideIndex += slidesToSlide;
+    if (slideIndex > x.length - slidesPerView) {
+        slideIndex = 0;
+    }
+    else if (slideIndex < 0) {
+        slideIndex = x.length - slidesPerView;
+    }
+    showDivs(slideIndex);
+}
+
+function showDivs(n) {
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = n; i < n + slidesPerView; i++) {
+        x[i].style.display = "block";
+    }
+}
+
+function plusDivs(n) {
+    swipeDivs();
+}
+showDivs(slideIndex);
 
 
 
